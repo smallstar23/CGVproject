ff803db8004a9b6a2b7906d9b682fe14003f066a
sunny.kim <smallstar232@gmail.com>
2022-02-28 16:09:18 +0900

    first commit

---
+++ src/main/java/com/koreait/cgvproject/CgVprojectApplication.java
@@ -0,0 +1,13 @@
+package com.koreait.cgvproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgVprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgVprojectApplication.class, args);
+    }
+
+}
