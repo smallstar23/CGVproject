b136984796838f32eec6ffd2c3615141f7817ea9
김태훈 <bfalcom@naver.com>
2022-03-02 23:46:01 +0900

    회원가입 로그인 mycgv support 컨트롤러

--- src/main/java/com/koreait/cgvproject/entity/MemberinfoEntity.java
+++ src/main/java/com/koreait/cgvproject/entity/MemberinfoEntity.java
@@ -39,5 +39,115 @@ public class MemberinfoEntity {
     private LocalDateTime updateDate;
     private Long valpoint;
 
+    public Long getIdx() {
+        return idx;
+    }
 
+    public void setIdx(Long idx) {
+        this.idx = idx;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserpw() {
+        return userpw;
+    }
+
+    public void setUserpw(String userpw) {
+        this.userpw = userpw;
+    }
+
+    public String getSsn1() {
+        return ssn1;
+    }
+
+    public void setSsn1(String ssn1) {
+        this.ssn1 = ssn1;
+    }
+
+    public String getSsn2() {
+        return ssn2;
+    }
+
+    public void setSsn2(String ssn2) {
+        this.ssn2 = ssn2;
+    }
+
+    public String getHp() {
+        return hp;
+    }
+
+    public void setHp(String hp) {
+        this.hp = hp;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getAddress3() {
+        return address3;
+    }
+
+    public void setAddress3(String address3) {
+        this.address3 = address3;
+    }
+
+    public LocalDateTime getRegdate() {
+        return regdate;
+    }
+
+    public void setRegdate(LocalDateTime regdate) {
+        this.regdate = regdate;
+    }
+
+    public LocalDateTime getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(LocalDateTime updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public Long getValpoint() {
+        return valpoint;
+    }
+
+    public void setValpoint(Long valpoint) {
+        this.valpoint = valpoint;
+    }
 }
