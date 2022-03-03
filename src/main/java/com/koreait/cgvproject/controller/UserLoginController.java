96e36b5d00e97ad666ba4ad00ff09d37e8f98d45
jhg9205 <jhg9205@nate.com>
2022-03-04 02:56:18 +0900

    (폴더,파일정리) (nav쪼개기) (컨트롤러경로수정)

---
+++ src/main/java/com/koreait/cgvproject/controller/UserLoginController.java
@@ -0,0 +1,33 @@
+package com.koreait.cgvproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserLoginController {
+
+    @GetMapping("/user/login")
+    public String login(){
+        return "user/login/login";
+    }
+
+    @GetMapping("/user/login-agreement")
+    public String login_agreement(){
+        return "user/login/login-agreement";
+    }
+
+    @GetMapping("/user/login-guest-hometicket")
+    public String login_guest_hometicket(){
+        return "user/login/login-guest-hometicket";
+    }
+
+    @GetMapping("/user/login-guest-login")
+    public String login_guest_login(){
+        return "user/login/login-guest-login";
+    }
+
+    @GetMapping("/user/login-guest-reserve")
+    public String login_guest_reserve(){
+        return "user/login/login-guest-reserve";
+    }
+}
