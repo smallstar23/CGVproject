0e94c05d975aec02717e75bbddbae6eeb31e69f3
EpsteinKim <97489793+EpsteinKim@users.noreply.github.com>
2022-03-04 09:37:10 +0900

    컨트롤러 ROOT 생성 user-gift 약간 수정

--- src/main/java/com/koreait/cgvproject/controller/UserStoreController.java
+++ src/main/java/com/koreait/cgvproject/controller/UserStoreController.java
@@ -11,50 +11,52 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/culture-event")
 public class UserStoreController {
 
+    private final String ROOT = "user/culture-event/popcorn-store";
+
     @GetMapping("/popcorn-store")
     public ModelAndView main(){
-        return new ModelAndView("/user/culture-event/popcorn-store");
+        return new ModelAndView(ROOT);
     }
 
     @GetMapping("/popcorn-store/store-category")
     public String category(@RequestParam(value = "cno", required = false, defaultValue = "1") String cno, Model model){
         if(cno.equals("1")) System.out.println("default 카테고리 넘버 : " + cno);
         else System.out.println("Get으로 들어온 카테고리 넘버 : " + cno);
-        return "user/culture-event/popcorn-store/store-category";
+        return ROOT+"/store-category";
     }
 
     @GetMapping("/popcorn-store/product-detail")
     public String detail(){
-        return "user/culture-event/popcorn-store/product-detail";
+        return ROOT + "/product-detail";
     }
 
     @GetMapping("/popcorn-store/user-cart")
     public String userCart(){
-        return "user/culture-event/popcorn-store/user-cart";
+        return ROOT + "/user-cart";
     }
 
     @GetMapping("/popcorn-store/user-gift")
     public String userGift(){
-        return "user/culture-event/popcorn-store/user-gift";
+        return ROOT + "/user-gift";
     }
 
     @GetMapping("/popcorn-store/store-payment")
     public String giftPay(){
-        return "user/culture-event/popcorn-store/store-payment";
+        return ROOT + "/store-payment";
     }
 
     @GetMapping("/popcorn-store/purchase-confirm")
     public String directPay(){
-        return "user/culture-event/popcorn-store/purchase-confirm";
+        return ROOT + "/purchase-confirm";
     }
 
     @GetMapping("/popcorn-store/payment-successcomplete")
     public String success(){
-        return "user/culture-event/popcorn-store/payment-successcomplete";
+        return ROOT + "/payment-successcomplete";
     }
 
     @GetMapping("/popcorn-store/payment-failcomplete")
     public String fail(){
-        return "user/culture-event/popcorn-store/payment-failcomplete";
+        return ROOT + "/payment-failcomplete";
     }
 }
