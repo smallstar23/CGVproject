d8912d43e5a5f73479bd05b6d8a421d55d66d0c6
sjkyomg <sjkyomg@gmail.com>
2022-03-06 23:59:21 +0900

    화이팅 입니다

--- src/main/java/com/koreait/cgvproject/repository/MovieRepository.java
+++ src/main/java/com/koreait/cgvproject/repository/MovieRepository.java
@@ -4,14 +4,7 @@ import com.koreait.cgvproject.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
-
-    //select * from movie where rownum<=? order by idx asc ;
-
-    List<Movie> findAll();
-
-
 }
