d9adaa7fe09478cb55f9640337d86d5516ed6470
sunny.kim <smallstar232@gmail.com>
2022-03-08 00:10:52 +0900

    03-07 admin 메인 수정중

--- src/main/java/com/koreait/cgvproject/controller/user/rest/AdminTheaterRestController.java
+++ src/main/java/com/koreait/cgvproject/controller/user/rest/AdminTheaterRestController.java
@@ -11,21 +11,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 @Controller
 public class AdminTheaterRestController {
 
-    @Autowired
-    private AdminTheaterService adminTheaterService;
-
-    @PostMapping("/login")
-    private String login(@RequestParam("tname") String tname, HttpServletRequest request){
-        System.out.println("전달 받은 theater name:" + tname);
-        HttpSession session= request.getSession();
-        session.setAttribute("tname", tname);
-        return "ok";
-    }
 
 
 
