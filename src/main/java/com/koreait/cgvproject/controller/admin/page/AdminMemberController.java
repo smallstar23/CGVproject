bb85319fc6405d2b803c862ce858a904e3b4db0a
EpsteinKim <97489793+EpsteinKim@users.noreply.github.com>
2022-03-07 23:31:02 +0900

    Session 전달 성...공...살..려...줘....

--- src/main/java/com/koreait/cgvproject/controller/admin/page/AdminMemberController.java
+++ src/main/java/com/koreait/cgvproject/controller/admin/page/AdminMemberController.java
@@ -1,80 +1,80 @@
-//package com.koreait.cgvproject.controller.admin.page;
-//
-//
-//import com.koreait.cgvproject.dto.Member2DTO;
-//import com.koreait.cgvproject.service.admin.service.MemberService;
-//import lombok.AllArgsConstructor;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PostMapping;
-//
-//import java.util.List;
-//
-//@AllArgsConstructor
-//@Controller
-//public class AdminMemberController {
-//
-//    private MemberService memberService;
-//
-//
-//    @GetMapping("member-lookup")//member-lookup 페이지 회원정보 조회
-//    public String member_lookup(Model model){
-//        List<Member2DTO> member2DTOList =memberService.getMember2List();
-//        model.addAttribute("member2DTOList",member2DTOList);
-//        return "/admin/member/member-lookup";
-//    }
-//    @PostMapping("/post")
-//    public  String write(Member2DTO member2DTO){
-//        memberService.insertPost(member2DTO);
-//        return  "redirect:/";
-//    }
-//
-//    @GetMapping("/post/{id}")
-//    public  String view(@PathVariable("id") Long id,Model model){
-//        Member2DTO member2DTO =memberService.getPost(id);
-//        model.addAttribute("post",member2DTO);
-//        return "admin/member/member_user_view";
-//    }
-//
-//
-//
-//    @GetMapping("member_user_view")//manage_user_view 페이지
-//    public String member_user_view(){
-//        return "/admin/member/member_user_view";
-//    }
-//
-//    @GetMapping("member-ticket")//member-ticket 페이지 예매내역 조회
-//    public String member_ticket(){
-//        return "/admin/member/member-ticket";
-//    }
-//
-//    @GetMapping("member-ticketing-view")//member-ticket-view 페이지
-//    public String member_ticket_view(){
-//        return "/admin/member/member_ticketing_view";
-//    }
-//
-//
-//    @GetMapping("member-paymentlist")//member-paymentlist 페이지 결제내역 조회
-//    public String member_paymentlist(){
-//        return "/admin/member/member-paymentlist";
-//    }
-//
-//    @GetMapping("member-paymentlist-view")//member-paymentlist-view 페이지
-//    public String member_paymentlist_view(){
-//        return "/admin/member/member-paymentlist-view";
-//    }
-//
-//
-//    @GetMapping("member-pointlist")//member-pointlist 페이지 포인트 내역 조회
-//    public String member_pointlist(){
-//        return "/admin/member/member-pointlist";
-//    }
-//
-//
-//    @GetMapping("member-pointlist-view")//member-pointlist-view 페이지
-//    public String member_pointlist_view(){
-//        return "/admin/member/member-pointlist-view";
-//    }
-//}
+package com.koreait.cgvproject.controller.admin.page;
+
+
+import com.koreait.cgvproject.dto.Member2DTO;
+import com.koreait.cgvproject.service.admin.service.MemberService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Controller
+public class AdminMemberController {
+
+    private MemberService memberService;
+
+
+    @GetMapping("member-lookup")//member-lookup 페이지 회원정보 조회
+    public String member_lookup(Model model){
+        List<Member2DTO> member2DTOList =memberService.getMember2List();
+        model.addAttribute("member2DTOList",member2DTOList);
+        return "/admin/member/member-lookup";
+    }
+    @PostMapping("/post")
+    public  String write(Member2DTO member2DTO){
+        memberService.insertPost(member2DTO);
+        return  "redirect:/";
+    }
+
+    @GetMapping("/post/{id}")
+    public  String view(@PathVariable("id") Long id,Model model){
+        Member2DTO member2DTO =memberService.getPost(id);
+        model.addAttribute("post",member2DTO);
+        return "admin/member/member_user_view";
+    }
+
+
+
+    @GetMapping("member_user_view")//manage_user_view 페이지
+    public String member_user_view(){
+        return "/admin/member/member_user_view";
+    }
+
+    @GetMapping("member-ticket")//member-ticket 페이지 예매내역 조회
+    public String member_ticket(){
+        return "/admin/member/member-ticket";
+    }
+
+    @GetMapping("member-ticketing-view")//member-ticket-view 페이지
+    public String member_ticket_view(){
+        return "/admin/member/member_ticketing_view";
+    }
+
+
+    @GetMapping("member-paymentlist")//member-paymentlist 페이지 결제내역 조회
+    public String member_paymentlist(){
+        return "/admin/member/member-paymentlist";
+    }
+
+    @GetMapping("member-paymentlist-view")//member-paymentlist-view 페이지
+    public String member_paymentlist_view(){
+        return "/admin/member/member-paymentlist-view";
+    }
+
+
+    @GetMapping("member-pointlist")//member-pointlist 페이지 포인트 내역 조회
+    public String member_pointlist(){
+        return "/admin/member/member-pointlist";
+    }
+
+
+    @GetMapping("member-pointlist-view")//member-pointlist-view 페이지
+    public String member_pointlist_view(){
+        return "/admin/member/member-pointlist-view";
+    }
+}
--- src/main/java/com/koreait/cgvproject/controller/admin/page/AdminMovieController.java
+++ src/main/java/com/koreait/cgvproject/controller/admin/page/AdminMovieController.java
@@ -1,83 +1,83 @@
-//package com.koreait.cgvproject.controller.admin.page;
-//
-//
-//import com.koreait.cgvproject.dto.MovieDTO;
-//import com.koreait.cgvproject.service.admin.service.MovieService;
-//import lombok.AllArgsConstructor;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//
-//import java.util.List;
-//
-//@AllArgsConstructor
-//@Controller
-//public class AdminMovieController {
-//    private MovieService movieService;
-//
-//
-//    @GetMapping("manage_movieSchedule")//movie-schedule 페이지
-//    public String movie_schedule(){
-//        return "/admin/movie/manage_movieSchedule";
+package com.koreait.cgvproject.controller.admin.page;
+
+
+import com.koreait.cgvproject.dto.MovieDTO;
+import com.koreait.cgvproject.service.admin.service.MovieService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Controller
+public class AdminMovieController {
+    private MovieService movieService;
+
+
+    @GetMapping("manage_movieSchedule")//movie-schedule 페이지
+    public String movie_schedule(){
+        return "/admin/movie/manage_movieSchedule";
+    }
+
+    @GetMapping("/manage_movieSchedule_create")//movie-schedule 페이지
+    public String movie_schedule_create(){
+        return "/admin/movie/manage_movieSchedule_create";
+    }
+
+    @PostMapping("/manage_movieSchedule_create")
+    public String writes(MovieDTO movieDTO){
+        movieService.insertPoint(movieDTO);
+        return "redirect:/";
+    }
+
+
+    @GetMapping("manage_pricem")//movie-pricem 페이지
+        public String movie_pricem(){
+        return "/admin/movie/manage_pricem";
+    }
+
+    @GetMapping("manage_pricerm_create")//movie-pricem 페이지
+    public String movie_pricem_create(){
+        return "/admin/movie/manage_pricerm_create";
+    }
+
+    @GetMapping("/manage_theaters")//movie-theaters 페이지
+    public String movie_theaters(Model model){
+
+//        List<HallDTO> hallDTOList = adminHallService.getHallList();
+//        model.addAttribute("HallList",hallDTOList);
+
+        return "/admin/movie/manage_theaters";
+    }
+
+//    @GetMapping("/manage_theaters_create")//movie-theaters 페이지
+//    public String movie_theaters_create(){
+//        return "/admin/movie/manage_theaters_create";
 //    }
-//
-//    @GetMapping("/manage_movieSchedule_create")//movie-schedule 페이지
-//    public String movie_schedule_create(){
-//        return "/admin/movie/manage_movieSchedule_create";
-//    }
-//
-//    @PostMapping("/manage_movieSchedule_create")
-//    public String writes(MovieDTO movieDTO){
-//        movieService.insertPoint(movieDTO);
+
+//    @PostMapping("/manage_theaters_create")
+//    public String movie_theaters_write(HallDTO hallDTO){
+//        adminHallService.savePost(hallDTO);
 //        return "redirect:/";
 //    }
-//
-//
-//    @GetMapping("manage_pricem")//movie-pricem 페이지
-//        public String movie_pricem(){
-//        return "/admin/movie/manage_pricem";
-//    }
-//
-//    @GetMapping("manage_pricerm_create")//movie-pricem 페이지
-//    public String movie_pricem_create(){
-//        return "/admin/movie/manage_pricerm_create";
-//    }
-//
-//    @GetMapping("/manage_theaters")//movie-theaters 페이지
-//    public String movie_theaters(Model model){
-//
-////        List<HallDTO> hallDTOList = adminHallService.getHallList();
-////        model.addAttribute("HallList",hallDTOList);
-//
-//        return "/admin/movie/manage_theaters";
-//    }
-//
-////    @GetMapping("/manage_theaters_create")//movie-theaters 페이지
-////    public String movie_theaters_create(){
-////        return "/admin/movie/manage_theaters_create";
-////    }
-//
-////    @PostMapping("/manage_theaters_create")
-////    public String movie_theaters_write(HallDTO hallDTO){
-////        adminHallService.savePost(hallDTO);
-////        return "redirect:/";
-////    }
-//    @GetMapping("manage_ongoingmovies")//movie-ongoing 페이지
-//    public String movie_ongoing(Model model){
-//        List<MovieDTO> movieDTOList =movieService.getMovieList();
-//        model.addAttribute("MovieList",movieDTOList);
-//        return "/admin/movie/manage_ongoingmovies";
-//    }
-//
-//    @GetMapping("/manage_ongoingmovies_create")//movie-ongoing 페이지
-//    public String movie_ongoing_create(){
-//        return "/admin/movie/manage_ongoingmovies_create";
-//    }
-//
-//    @PostMapping("/manage_ongoingmovies_create")
-//    public String write(MovieDTO movieDTO){
-//        movieService.insertPoint(movieDTO);
-//        return "redirect:/";
-//    }
-//}
+    @GetMapping("manage_ongoingmovies")//movie-ongoing 페이지
+    public String movie_ongoing(Model model){
+        List<MovieDTO> movieDTOList =movieService.getMovieList();
+        model.addAttribute("MovieList",movieDTOList);
+        return "/admin/movie/manage_ongoingmovies";
+    }
+
+    @GetMapping("/manage_ongoingmovies_create")//movie-ongoing 페이지
+    public String movie_ongoing_create(){
+        return "/admin/movie/manage_ongoingmovies_create";
+    }
+
+    @PostMapping("/manage_ongoingmovies_create")
+    public String write(MovieDTO movieDTO){
+        movieService.insertPoint(movieDTO);
+        return "redirect:/";
+    }
+}
