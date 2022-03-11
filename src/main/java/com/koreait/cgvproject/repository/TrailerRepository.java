b8b3203a0638bb689117103b66b49de2fca0aba7
sjkyomg <sjkyomg@gmail.com>
2022-03-11 11:59:29 +0900

    영화 등록 수정 중 폼 수정완료

--- src/main/java/com/koreait/cgvproject/repository/TrailerRepository.java
+++ src/main/java/com/koreait/cgvproject/repository/TrailerRepository.java
@@ -1,7 +1,9 @@
 package com.koreait.cgvproject.repository;
 
+import com.koreait.cgvproject.entity.Movie;
 import com.koreait.cgvproject.entity.Theater;
 import com.koreait.cgvproject.entity.Trailer;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +11,8 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
+
 public interface TrailerRepository extends JpaRepository<Trailer, Long> {
-    Trailer findByMcode(Long mcode);
+    Trailer findByMovie(Long movie);
 
 }
