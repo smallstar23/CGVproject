93172c3d3f66b7d1809ffbe182653be4346f489b
sunny.kim <smallstar232@gmail.com>
2022-03-13 01:33:56 +0900

    03-12 user movies 순서 따로 틀만 잡음

--- src/main/java/com/koreait/cgvproject/controller/user/page/UserTheatersController.java
+++ src/main/java/com/koreait/cgvproject/controller/user/page/UserTheatersController.java
@@ -1,6 +1,8 @@
 package com.koreait.cgvproject.controller.user.page;
 
+import com.koreait.cgvproject.dto.HallDTO;
 import com.koreait.cgvproject.dto.TheaterDTO;
+import com.koreait.cgvproject.entity.Hall;
 import com.koreait.cgvproject.service.admin.theater.AdminTheaterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,15 +21,18 @@ public class UserTheatersController {
     @GetMapping("/theaters")
     public String theaters(Model model){
         List<TheaterDTO> theaterDTOList=adminTheaterService.getCGV(1L);
+        List<HallDTO> lengthhalls=adminTheaterService.getHallList(theaterDTOList.get(0).getTcode());
         model.addAttribute("theater",theaterDTOList.get(0));
-        model.addAttribute("theaterList",  theaterDTOList);
+        model.addAttribute("lengthhalls", lengthhalls.size());
         return "user/theaters/theaters";
     }
 
     @GetMapping("/theaters/{tcode}")
     public String theatersview(@PathVariable Long tcode, Model model){
         TheaterDTO theaterDTO=adminTheaterService.getTheater(tcode);
+        List<HallDTO> lengthhalls=adminTheaterService.getHallList(tcode);
         model.addAttribute("theater", theaterDTO);
+        model.addAttribute("lengthhalls", lengthhalls.size());
         return "/user/theaters/theaters";
     }
 
