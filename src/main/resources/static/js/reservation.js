e56f87c80758009bd48b1dc01528a4413f819da0
EpsteinKim <97489793+EpsteinKim@users.noreply.github.com>
2022-03-18 18:06:04 +0900

    reservation.js추가

--- src/main/resources/static/js/reservation.js
+++ src/main/resources/static/js/reservation.js
@@ -422,4 +422,64 @@ $(".clickPoint").on({
     }
 })
 
+// EpsteinKim의 reservation_EpsteinKim.js
+const alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+showSeat('H','8','0','0');
+seatInit();
 
+function showSeat(rowVal, colVal, em_rowVal, em_colVal) {
+    let sHtml = "";
+    let st_row = convertNumber(rowVal) + 1; //seatHTML L == 12
+    let st_col = colVal;    //seatHtml st_col 19
+
+    let st_row_empty = em_rowVal.replaceAll(' ', '').toUpperCase().trim().split(','); //database G == 7
+    let cnt = 0;
+    st_row_empty.forEach(e_r => st_row_empty[cnt++] = String(convertNumber(e_r) + 1))
+
+    let st_col_empty = em_colVal.replaceAll(' ', '').trim().split(',');  //database
+
+    for (let row = 0; row < st_row; row++) {
+        // 가로 좌석 생성
+        sHtml += `<div id="${convertAlpha(row)}" class="row_div">`;
+        sHtml += `<div class="row-text">${convertAlpha(row)}</div>`;
+        for (let col = 0; col < st_col; col++) {
+            // 세로 좌석 생성
+            sHtml += `<div class="seat" id="${convertAlpha(row) + Number(col + 1)}">${col + 1}</div>`
+            // 세로 통로 생성 부분
+            st_col_empty.forEach(function (emptyCol) {
+                if (emptyCol == col + 1) {
+                    sHtml += `<div class="empty"></div>`;
+                }
+            })
+        }
+        sHtml += `</div>`;
+        // 가로 통로 생성 부분
+        st_row_empty.forEach(emptyRow => {
+            if (emptyRow == row + 1) {
+                sHtml += `<div class="empty"></div>`;
+            }
+        })
+    }
+    // 대상 id를 가지고 있는 컴포넌트
+    document.getElementById('seats').innerHTML = sHtml;
+}
+
+function seatInit() {
+    const allSeat = document.getElementsByClassName('seat');
+    for (let i = 0; i < allSeat.length; i++) {
+        let seat = allSeat[i];
+
+        seat.addEventListener('click', function () {
+            if (seat.classList.contains('selected')) seat.classList.remove('selected');
+            else seat.classList.add('selected');
+        })
+    }
+}
+
+function convertAlpha(number) {
+    return alpha[Number(number)];
+}
+
+function convertNumber(alph) {
+    return Number(alpha.indexOf(alph));
+}
