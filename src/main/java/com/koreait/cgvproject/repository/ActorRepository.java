a87cb96de6349b694469106a70e46018cde5c686
sjkyomg <sjkyomg@naver.com>
2022-03-22 23:55:18 +0900

    무비 상세페이지 감독/출연 수정 완료

--- src/main/java/com/koreait/cgvproject/repository/ActorRepository.java
+++ src/main/java/com/koreait/cgvproject/repository/ActorRepository.java
@@ -1,11 +1,17 @@
 package com.koreait.cgvproject.repository;
 
+import com.koreait.cgvproject.dto.ActorDTO;
 import com.koreait.cgvproject.entity.Actor;
 import com.koreait.cgvproject.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ActorRepository extends JpaRepository <Actor ,Long> {
 
     Actor findByMovie(Movie movie);
 
+    @Query(value = "SELECT * FROM Actor where mcode = :mcode", nativeQuery = true)
+    List<Actor> findByMcode(Long mcode);
 }
